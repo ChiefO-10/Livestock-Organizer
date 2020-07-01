d6006f4800da573b892cdf091082b90d0afe1aef
rafał kucharski <draer10@gmail.com>
2020-07-01 21:34:45 +0200

    Folders cleanup

--- Livestock Data Access/Properties/AssemblyInfo.cs
+++ Livestock Data Access/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LivestockDataAccess")]
+[assembly: AssemblyDescription("Class library for database data access")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("LivestockDataAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
